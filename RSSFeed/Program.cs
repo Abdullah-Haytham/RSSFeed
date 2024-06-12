957aee6bcee0fba12fe5e11c1a2d8ebba12c5984
Abdullah-Haytham <hedeya@gmail.com>
2024-06-12 20:30:35 +0300

    Finished the login and register pages, Added add feed and delete feed modals

--- RSSFeed/Program.cs
+++ RSSFeed/Program.cs
@@ -1,9 +1,33 @@
+
+
+using Dapper;
+using Microsoft.Data.Sqlite;
+using System.Data;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddAntiforgery();
+builder.Services.AddSingleton<IDbConnection>(_ => new SqliteConnection("Data Source=wwwroot/linksdb.db"));
+
 var app = builder.Build();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
+app.MapFallbackToFile("/index.html");
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/shortcuts", async (IDbConnection db) =>
+{
+    var sql = "SELECT name FROM feeds WHERE userId=1"; //user id will need to be dynamic (use cookies 8aleban)
+    var names = await db.QueryAsync(sql);
+
+    StringBuilder shortcuts = new StringBuilder();
+    foreach (var name in names)
+    {
+        shortcuts.Append(@$"<a class=""shortcut"">{name.name}</a>");
+    }
+    return Results.Content(shortcuts.ToString());
+});
 
 app.Run();
