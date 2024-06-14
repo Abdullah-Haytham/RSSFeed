d83bff68baa8a5306934fea02802ced638d07f61
Abdullah-Haytham <hedeya@gmail.com>
2024-06-14 14:10:14 +0300

    updated the "/" route and added login route that stores cookies

--- RSSFeed/Program.cs
+++ RSSFeed/Program.cs
@@ -2,6 +2,7 @@
 
 using Dapper;
 using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using System.Data;
 using System.ServiceModel.Syndication;
@@ -33,6 +34,24 @@ app.MapGet("/token", (HttpContext context, IAntiforgery antiforgery) =>
     var token = antiforgery.GetAndStoreTokens(context);
     string html = $"""<input name = "{token.FormFieldName}" type = "hidden" value = "{token.RequestToken}"/>""";
     return Results.Content(html, "text/html");
+});
+
+app.MapPost("/login", async (HttpContext context, IAntiforgery antiforgery, IDbConnection db, [FromForm] string email, [FromForm] string password) =>
+{
+    await antiforgery.ValidateRequestAsync(context);
+
+    string sql = "SELECT COUNT(*) AS UserCount FROM users WHERE email = @Email AND password = @Password";
+    var result = await db.QueryFirstOrDefaultAsync<int>(sql, new { Email = email, Password = password });
+
+    if(result == 1)
+    {
+        sql = "SELECT userId FROM users WHERE email = @Email AND password = @Password";
+        var userId = await db.QueryFirstOrDefaultAsync<int>(sql, new { Email = email, Password = password });
+        context.Response.Cookies.Append("UserId", userId.ToString(), cookieOptions);
+        return Results.Ok("User Logged in successfully");
+    }
+
+    return Results.Content("Ay 7aga for now", "text/html");
 });
 
 app.MapGet("/shortcuts", async (IDbConnection db) =>
@@ -113,38 +132,11 @@ app.MapGet("/feeds", async (IDbConnection db, HttpContext context) =>
     return Results.Content(feedBuilder.ToString());
 });
 
-app.MapGet("/", async (HttpContext context, IDbConnection db) =>
+app.MapGet("/check-page", async (HttpContext context, IDbConnection db) =>
 {
     if (context.Request.Cookies.TryGetValue("UserId", out var userId))
     {
         var htmlContent = """
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width">
-                <title></title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-                <link rel="stylesheet" href="styles.css">
-
-                <link rel="preconnect" href="https://fonts.googleapis.com">
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&display=swap" rel="stylesheet">
-
-                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-                    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-                    crossorigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-                    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-                    crossorigin="anonymous"></script>
-                <script src="https://unpkg.com/htmx.org@1.9.12"></script>
-                <script src="script.js" defer></script>
-
-            </head>
-
-            <body>
-                <div class="replace">
                     <header class="header">
                         <div class="navbar d-flex align-items-center">
                             <button class="menu"><img class="menu-img" src="images/menu-svgrepo-com.svg" alt="menu button"></button>
@@ -153,7 +145,7 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
                         </div>
                     </header>
 
-                    <main class="main-container replace">
+                    <main class="main-container">
 
                         <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
                             <div class="modal-dialog">
@@ -166,7 +158,7 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
 
                                     <div class="modal-body">
                                         <form action="/login" method="post" class="modal-form">
-                                            <div hx-get="/token" hx-trigger="load" hx-swap="outerHTML">
+                                            <div hx-get="/token" hx-trigger="load" hx-swap="innerHTML"></div>
                                             <div class="form-group">
                                                 <label for="name">Name</label>
                                                 <input class="form-control" type="text" id="name" name="name" placeholder="Name" />
@@ -195,7 +187,7 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
 
                                     <div class="modal-body">
                                         <form action="/login" method="post" class="modal-form">
-                                            <div hx-get="/token" hx-trigger="load" hx-swap="outerHTML">
+                                            <div hx-get="/token" hx-trigger="load" hx-swap="innerHTML"></div>
                                             <div class="form-group mb-3">
                                                 <label for="select">Feed Name</label>
                                                 <select hx-get="/select-options" hx-trigger="load" hx-swap="innerHTML" class="form-select" aria-label="Default select example">
@@ -235,54 +227,13 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
                             Copyright 2024 ©Abdullah Haytham Hedeya
                         </div>
                     </footer>
-                </div>
-            </body>
-
-            <script>
-                const menu = document.querySelector('.menu');
-                const sidebar = document.querySelector('.sidebar-container');
-                const main = document.querySelector('.main-container');
-
-                menu.addEventListener('click', () => {
-                    sidebar.classList.toggle('active');
-                    main.classList.toggle('no-grid')
-                });
-            </script>
-        </html>
         """;
         return Results.Content(htmlContent, "text/html");
     }
     else
     {
         var htmlContent = """
-                <!DOCTYPE html>
-        <html>
-
-        <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width">
-            <title></title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-            <link rel="stylesheet" href="styles.css">
-
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles:wght@400;700&display=swap" rel="stylesheet">
-
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-                integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-                crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-                integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-                crossorigin="anonymous"></script>
-            <script src="https://unpkg.com/htmx.org@1.9.12"></script>
-            <script src="script.js" defer></script>
-
-        </head>
-
-        <body>
-
+         
             <header class="header">
                 <div class="navbar d-flex align-items-center">
                     <button class="menu"><img class="menu-img" src="images/menu-svgrepo-com.svg" alt="menu button"></button>
@@ -295,7 +246,7 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
                 <form action="/login" method="post" class="login" id="login-form">
 
                     <h1 style="text-align: center; margin-bottom: 24px;">Login</h1>
-                    <div hx-get="/token" hx-trigger="load" hx-swap="outerHTML">
+                    <div hx-get="/token" hx-trigger="load" hx-swap="innerHTML"></div>
                     <div class="form-group">
                         <label for="email">Email</label>
                         <input class="form-control" type="email" id="email" name="email" placeholder="Email" />
@@ -311,6 +262,11 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
                     <div style="margin-bottom: 8px;">Don't have an account? <button class="to-register-btn">Register</button></div>
 
                     <button class="btn btn-primary" type="submit">Login</button>
+                    <div class="alert-container">
+                        <div id="alert-box">
+                            invalid email or password
+                        </div>
+                    </div>
                 </form>
             </main>
 
@@ -319,13 +275,12 @@ app.MapGet("/", async (HttpContext context, IDbConnection db) =>
                     Copyright 2024 ©Abdullah Haytham Hedeya
                 </div>
             </footer>
-
-        </body>
-
-        </html>
         """;
         return Results.Content(htmlContent, "text/html");
     }
+
+    
 });
+
 
 app.Run();
--- RSSFeed/wwwroot/script.js
+++ RSSFeed/wwwroot/script.js
@@ -1,6 +1,8 @@
 const loginForm = document.getElementById("login-form");
 const emailVal = document.getElementsByClassName("email-val")[0]
 const passwordVal = document.getElementsByClassName("pass-val")[0]
+
+if(loginForm != null && emailVal != null && passwordVal != null){
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -19,4 +21,42 @@ loginForm.addEventListener('submit', (e) => {
     if (emailVal.innerHTML !== "" || passwordVal.innerHTML !== "") {
         return;
     }
+
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
+
+    fetch('/login', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (!response.ok){
+                console.log("Invalid credentials")
+                alertBox = document.getElementById("alert-box");
+                alertBox.classList.add("red")
+                alertBox.innerHTML = "Invalid credentials"
+            }else{
+                console.log("Login Successful")
+                alertBox = document.getElementById("alert-box");
+                alertBox.classList.add("green")
+                alertBox.innerHTML = "Login Successful!"
+            }
+        })
+        .catch(error => {
+            // handle error
+        });
 });
+}
+
+document.body.addEventListener('htmx:afterRequest', function (evt) {
+    if (evt.detail.requestConfig.path === "/login") {
+        console.log("tried login")
+        alertBox = document.getElementById("alert-box");
+        if (alertBox.classList.contains("green")) {
+            console.log("Login Successful")
+            HTMLTextAreaElement.ajax("GET", "/check-page", {target: ".replace"})
+        }
+    }
+}
+)
